49fc4423dfd9ba068903cd1616006bcf7e5a15de
Gabriek0 <gabriel.sanches170@gmail.com>
2022-08-27 12:04:32 -0300

    feat: refresh token and intercept request

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
-import { parseCookies } from "nookies";
+import { parseCookies, setCookie } from "nookies";
 
-const cookies = parseCookies();
+let cookies = parseCookies();
 
 export const api = axios.create({
   baseURL: "http://localhost:3333",
@@ -10,3 +10,48 @@ export const api = axios.create({
 api.defaults.headers.common[
   "Authorization"
 ] = `Bearer ${cookies["next.cubik.token"]}`;
+
+// Intercept request
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      if (error.response.data?.code === "token.expired") {
+        // renew token
+        cookies = parseCookies();
+
+        const { "next.cubik.refreshToken": refreshToken } = cookies;
+
+        api
+          .post("/refresh", {
+            refreshToken,
+          })
+          .then((response) => {
+            const { token } = response.data;
+
+            setCookie(undefined, "next.cubik.token", token, {
+              maxAge: 60 * 60 * 24 * 30, // 30 days
+              path: "/",
+            });
+
+            setCookie(
+              undefined,
+              "next.cubik.refreshToken",
+              response.data.refreshToken,
+              {
+                maxAge: 60 * 60 * 24 * 30, // 30 days
+                path: "/",
+              }
+            );
+
+            api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+          });
+      } else {
+        // user logout
+      }
+    }
+  }
+);
