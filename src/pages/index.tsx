921b29d958d7cd5c07fb8a6f727ed9a9cc3fe8db
Gabriek0 <gabriel.sanches170@gmail.com>
2022-08-30 21:49:53 -0300

    feat: add chakra-ui to login screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
-import type { GetServerSideProps, NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import { parseCookies } from "nookies";
+import type { NextPage } from "next";
 import { FormEvent, useContext, useState } from "react";
+import { Button, Flex, Input, Stack, Text } from "@chakra-ui/react";
 import { AuthContext } from "../contexts/AuthContext";
-import styles from "../styles/Home.module.css";
 import { withSSRGuest } from "../utils/withSSRGuest";
 
 const Home: NextPage = () => {
@@ -25,19 +22,55 @@ const Home: NextPage = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit} className={styles.container}>
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type="submit">Enviar</button>
-    </form>
+    <Flex h="100vh" w="100vw" align="center" justify="center" background="body">
+      <Flex
+        h="100%"
+        w="100%"
+        p="8"
+        as="form"
+        maxW={360}
+        flexDir="column"
+        align="center"
+        justify="center"
+        onSubmit={handleSubmit}
+      >
+        <Stack spacing={4} w="100%">
+          <Text
+            fontSize="24"
+            fontWeight="600"
+            color="purple.800"
+            textAlign="center"
+            mb="8"
+          >
+            LOGIN
+          </Text>
+          <Input
+            p="6"
+            type="email"
+            fontWeight="600"
+            background="gray.200"
+            placeholder="E-mail"
+            borderRadius="2"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            p="6"
+            fontWeight="600"
+            type="password"
+            placeholder="Password"
+            background="gray.200"
+            borderRadius="2"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <Button p="6" borderRadius="2" colorScheme="purple" type="submit">
+            LOGIN
+          </Button>
+        </Stack>
+      </Flex>
+    </Flex>
   );
 };
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    teal: {
+      "50": "#FAF5FF",
+      "200": "#D6BCFA",
+      "400": "#9F7AEA",
+      "500": "#805AD5",
+      "600": "#6B46C1",
+      "700": "#553C9A",
+      "800": "#44337A",
+    },
+  },
+  fonts: {},
+});
